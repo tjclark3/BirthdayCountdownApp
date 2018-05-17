906781e4fccdc8b1ec35654e336509ede05b7398
TJ Clark <mav@TJs-Mac-mini.local>
2018-05-17 10:12:34 -0700

    added skew to clock component

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <header>
+          <h1>It Is Your Birthday Countdown</h1>
+          <div className="header_skew">
+            <div className="header_subskew">
+                         
+            </div>
+          </div>
+         </header>
          <BirthdayForm />
       </div>
     );
